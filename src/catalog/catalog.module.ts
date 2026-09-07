131ecca4e64040aeebf846262661f83741012fb0
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-07 13:40:11 +0500

    Remove legacy NestJS catalog module

--- src/catalog/catalog.module.ts
+++
@@ -1,4 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatalogService } from './catalog.service';
-import { CatalogController } from './catalog.controller';
-@Module({ providers: [CatalogService], controllers: [CatalogController], exports: [CatalogService] }) export class CatalogModule {}
