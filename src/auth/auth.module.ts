91c8af537a92a3edbc91facc99bd3983130bbc26
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-07 13:40:01 +0500

    Remove legacy NestJS auth module

--- src/auth/auth.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
-import { AdminGuard } from '../common/guards/admin.guard';
-@Module({ imports: [ConfigModule, JwtModule.registerAsync({ imports: [ConfigModule], inject: [ConfigService], useFactory: (config: ConfigService) => ({ secret: config.getOrThrow<string>('JWT_SECRET'), signOptions: { expiresIn: '8h' } }) })], providers: [AuthService, AdminGuard], exports: [AuthService, AdminGuard, JwtModule] }) export class AuthModule {}
