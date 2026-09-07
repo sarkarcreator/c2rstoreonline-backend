5de2e41df65e599681745ac5b6d701e2733903c6
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-07 13:39:46 +0500

    Remove legacy NestJS affiliate module

--- src/affiliates/affiliates.module.ts
+++
@@ -1,3 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AffiliatesController } from './affiliates.controller';
-@Module({ controllers: [AffiliatesController] }) export class AffiliatesModule {}
