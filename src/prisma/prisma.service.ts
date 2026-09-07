3c7e1b148d06cdc249e50915ec66192683a4cf98
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-07 13:40:40 +0500

    Remove legacy NestJS Prisma service

--- src/prisma/prisma.service.ts
+++
@@ -1,7 +0,0 @@
-import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-@Injectable() export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  async onModuleInit() { await this.$connect(); }
-  async onModuleDestroy() { await this.$disconnect(); }
-  enableShutdownHooks(app: INestApplication) { this.$on('beforeExit' as never, async () => await app.close()); }
-}
