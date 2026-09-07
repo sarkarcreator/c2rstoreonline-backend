bd966815241a8500acb8ae4b80d0279f847fd8e2
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-07 13:39:39 +0500

    Remove legacy NestJS admin module

--- src/admin/admin.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminController } from './admin.controller';
-import { AuthModule } from '../auth/auth.module';
-import { CatalogModule } from '../catalog/catalog.module';
-@Module({ imports: [AuthModule, CatalogModule], controllers: [AdminController] }) export class AdminModule {}
